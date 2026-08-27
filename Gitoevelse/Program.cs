b710b5b63dec57f1afe1b4ad9291db26c9a66eec
Jyothsna Batta <jyothsnabatta@gmail.com>
2026-08-27 22:56:44 +0200

    Add new feature

--- Gitoevelse/Program.cs
+++ Gitoevelse/Program.cs
@@ -1,3 +1,4 @@
-﻿Console.WriteLine("Hello from A! Welcome to my Git exercise!");
+﻿Console.WriteLine("Hello from B! Welcome to my Git exercise!");
 Console.WriteLine("I am learning C# and Git.");
 Console.WriteLine("This is Lesson 1.");
+Console.WriteLine("Welcome to my new feature!");
